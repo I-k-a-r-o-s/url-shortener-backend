fc062d0f716c341daf6967e4eb827bc9309263ea
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-16 15:17:18 +0530

    added mongodb connection, set up cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import { mongoDB } from "./config/mongodb.js";
+import cors from "cors";
+
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  }),
+);
 
 const PORT = process.env.PORT;
 
 const startServer = async () => {
   try {
+    await mongoDB();
     server.listen(PORT, () => {
       console.log(`Server started on Port:- ${PORT}`);
     });
