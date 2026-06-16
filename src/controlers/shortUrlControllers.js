b831a2ad0ca63000923542ba20b8a7b4d3def1ab
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-16 16:33:03 +0530

    wrote controller functions

---
+++ src/controlers/shortUrlControllers.js
@@ -0,0 +1,85 @@
+import { errorResponse } from "../helpers/response.js";
+import ShortUrlModel from "../models/shortUrlModel.js";
+
+export const createUrl = async (req, res) => {
+  try {
+    const { fullUrl } = req.body;
+
+    const urlFound = await ShortUrlModel.find({ fullUrl });
+    if (!urlFound) {
+      return res.status(409).json({
+        success: false,
+        message: "URL already exists!",
+        urlFound,
+      });
+    }
+
+    const shortUrl = await ShortUrlModel.create({ fullUrl });
+    return res.status(201).json({
+      success: true,
+      message: "Successfully created the URL!",
+      shortUrl,
+    });
+  } catch (error) {
+    return errorResponse(res, "createUrl", error);
+  }
+};
+
+export const getAllUrl = async (req, res) => {
+  try {
+    const shortUrls = await ShortUrlModel.find();
+    if (!shortUrls) {
+      return res.status(404).json({
+        success: false,
+        message: "No URLs found!",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Successfully fetched URLs!",
+      shortUrls,
+    });
+  } catch (error) {
+    return errorResponse(res, "getAllUrl", error);
+  }
+};
+
+export const getUrl = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const shortUrl = await ShortUrlModel.findOne({ shortUrl: id });
+    if (!shortUrl) {
+      return res.status(404).json({
+        success: false,
+        message: "Full URL not found!",
+      });
+    }
+
+    shortUrl.clicks++;
+    shortUrl.save();
+    return res.redirect(`${shortUrl.fullUrl}`);
+  } catch (error) {
+    return errorResponse(res, "getUrl", error);
+  }
+};
+
+export const deleteUrl = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const shortUrl = await ShortUrlModel.findByIdAndDelete({ _id: id });
+    if (!shortUrl) {
+      return res.status(204).json({
+        success: false,
+        message: "URL not found!",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "URL deleted successfully!",
+    });
+  } catch (error) {
+    return errorResponse(res, "deleteUrl", error);
+  }
+};
